c81b991fd847ea0cf54d85863d8396c620a123b7
Alexandra Svyatova <alexsvyatova@gmail.com>
2015-04-06 22:48:25 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,17 @@
 
 int main()
 {
+	int hight, width;
+	std::cin >>hight;
+	std::cin >> width;
+	//создаем мир
+	World MyWorld(hight, width);
 
+	//как мы зададим живые клетки? мышью хотели. тогда уже нужно Qt юзать? 
+	while (MyWorld.replay())
+	{
+		MyWorld.doStep();
+	}
 	return 0;
 
 }
