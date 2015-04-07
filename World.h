9638e27adb9e6b3f2121675fad26b3c1232b4120
Alexandra Svyatova <alexsvyatova@gmail.com>
2015-04-07 17:07:26 +0600

    Update World.h

--- World.h
+++ World.h
@@ -4,12 +4,16 @@
 
 class World
 {
+private:
 	unsigned int hight; //only positive
 	unsigned int width;
 	Cell** AllCells;
 
 public:
 	World(unsigned int hight, unsigned int width);
+	~World();
 	bool replay();
 	void doStep();
+	Cell setStatusofCell(int x, int y, bool alive);
+	bool getStatusofCell(int x, int y);
 };
