259a5ab85ea859e78b9e166e4c743b7d2835e70d
Alexandra Svyatova <alexsvyatova@gmail.com>
2015-04-11 00:24:34 +0600

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
 #include "Cell.h"
 
-//создаем мертвую клетку
+
+//create new alive cell
 Cell::Cell()
 {
 	alive = true;
 }
 
-//функция оживления или умертвления клетки
+
+//function of the step to the future, 
+//define what will be the status of cell in the future
 bool Cell::doStep()
 {
-	//проверяем сумму живых соседей
+	//calculate the sum of the neighbors
 	int sum = 0;
 	for (int i = 0; i < cell_neighbors.size(); i++)
 	{
+		//if the cell is alive "plus one" to the sum
 		sum += cell_neighbors[i]->alive;
 	}
+
+
+	//if the sum of the neighbors satisfy our criteria 
+	//than the cell become alive
 	if (sum == 3 || sum == 2) alive = true;
 	else alive = false;
+
+
+	//return the status of cell
 	return alive;
 }
 
+
+//recognize the cell's status
 bool Cell::getStatus() { return alive; }
-bool Cell::setStatus(bool alive_)
+Cell* Cell::who_are_you() { return this; }
+
+//give to the cell a certain status
+void Cell::setStatus(bool alive)
 {
-	alive = alive_;
-	return alive;
+	this->alive = alive;
 }
 
