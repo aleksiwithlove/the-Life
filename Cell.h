78e6f34d7376ff526208d61751726ed22a5c292e
Alexandra Svyatova <alexsvyatova@gmail.com>
2015-05-22 13:41:02 +0600

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -8,10 +8,12 @@ public:
 	std::vector<Cell*> cell_neighbors;
 
 	bool alive;
+    bool newAlive;
 
 	Cell();
 	bool getStatus();
 	void setStatus(bool alive);
-	bool doStep();
+    bool calculateNew();
+    bool returnNew();
     void addNeighbour(Cell* n);
 };
