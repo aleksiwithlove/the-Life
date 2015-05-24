fc4eccd36e09403963e02f2d47268c2ac16d4878
Alexandra Svyatova <alexsvyatova@gmail.com>
2015-05-25 01:42:56 +0600

    Update World.h

--- World.h
+++ World.h
@@ -5,13 +5,18 @@
 
 class World
 {
-public:
+private:
     unsigned int height; //only positive
     unsigned int width;
     std::vector<std::vector<Cell*>> AllCells;
+
+public:
+
     World(unsigned int height, unsigned int width);
 	~World();
     void doStep();
+    int WorldHeight();
+    int WorldWidth();
 
     Cell* setStatusOfCell(int x, int y, bool alive);
     bool getStatusOfCell(int x, int y);
