322d286df12f5e83cb2a0506a3acf5eb4471fc3e
Alexandra Svyatova <alexsvyatova@gmail.com>
2015-05-25 02:40:52 +0600

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -1,28 +1,23 @@
 #include <iostream>
 #include "Cell.h"
 
-
 Cell::Cell()
 {
     alive = false;
 }
 
-bool Cell::calculateNew()
+bool Cell::calculateNewStatus()
 {
     int sum = 0;
     for (int i = 0; i < cell_neighbors.size(); i++)
     {
         sum += cell_neighbors[i]->getStatus() ? 1 : 0;
     }
-
-
-
     if (alive)
     {
         newAlive = (sum == 3 || sum == 2);
     }
     else {newAlive = (sum == 3);}
-
     return newAlive;
 }
 
@@ -32,14 +27,14 @@ bool Cell::returnNew()
 }
 
 
-bool Cell::getStatus() {
+bool Cell::getStatus() 
+{
     return alive;
 }
 
 void Cell::addNeighbour(Cell* n)
 {
     cell_neighbors.push_back(n);
-
 }
 
 void Cell::setStatus(bool alive)
