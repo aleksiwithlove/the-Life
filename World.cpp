d9fc68a5aaa306817d1d4488d74608d7b0d023e3
Alexandra Svyatova <alexsvyatova@gmail.com>
2015-05-25 00:50:46 +0600

    Update World.cpp

--- World.cpp
+++ World.cpp
@@ -9,42 +9,37 @@ World::World(unsigned int height=20, unsigned int width=20)
 	this->width = width;
 
     AllCells.resize(height);
-    //Cell** AllCells = new Cell*[height];
+
 	for (int i = 0; i < height; i++)
 	{
         AllCells[i].resize(width);
 		for (int j = 0; j < width; j++)
 		{
-            Cell Ameba;
-            Ameba.setStatus(false);
+            Cell* Ameba = new Cell();
+            Ameba->setStatus(false);
             AllCells[i][j] = Ameba;
-
 		}
 	}
 
-	//so, let's find our neighbors
+    Cell* n;
     for (int i = 0; i < height; i++)
 	{
-		for (int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
 		{
 			for (int k = -1; k <= 1; k++)
 			{
 				for (int g = -1; g <= 1; g++)
 				{
-					if (i != 0 && j != 0 && i != height && j != width)
-					{
-                            {
-                          if (k != 0 && g != 0) {
-                              AllCells[i][j].addNeighbour(&(AllCells[(i+k)%height][(j+g)%width]));
-                          }
-						}
-					}
-				}
+                    if ((k == 0 && g == 0) == false)
+                    {
+                        n = AllCells[(i + k) % height][(j + g) % width];
+                        AllCells[i][j]->addNeighbour(n);
+                    }
+                }
 			}
 		}
 	}
 }
-
 
 
 //deconstructor of the world
@@ -59,40 +54,39 @@ void World::doStep()
 	{
 		for (int j = 0; j < width; j++)
 		{
-            AllCells[i][j].calculateNew();
+            AllCells[i][j]->calculateNew();
 		}
 	}
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            AllCells[i][j].setStatus(AllCells[i][j].returnNew());
+            AllCells[i][j]->setStatus(AllCells[i][j]->returnNew());
         }
     }
 
 }
 
 
-Cell World::setStatusOfCell(int x, int y, bool alive)
+Cell* World::setStatusOfCell(int x, int y, bool alive)
 {
-    AllCells[x][y].setStatus(alive);
+    AllCells[x][y]->setStatus(alive);
 	return AllCells[x][y];
 }
 
 
 bool World::getStatusOfCell(int x, int y)
 {
-    return AllCells[x][y].getStatus();
+    return AllCells[x][y]->getStatus();
 }
 
-//reset the world
 void World::reset()
 {
 	for (int i = 0; i < height; i++)
 	{
 		for (int j = 0; j < width; j++)
 		{
-			AllCells[i][j].setStatus(false);
+            AllCells[i][j]->setStatus(false);
 		}
 	}
 }
